f22facf478db0c036cf1a02c670f5a717a30a3fb
Edy Segura <edysegura@gmail.com>
2015-11-11 22:18:44 +0000

    Added validation

--- api/index.php
+++ api/index.php
@@ -35,7 +35,13 @@ $app->get('/tasks/:id', function($id) use ( $app ) {
 $app->post('/tasks', function() use ( $app ) {
     $taskJson = $app->request()->getBody();
     $task = json_decode($taskJson);
-    echo $task->description;
+    if($task) {
+        echo "{$task->description} added";
+    }
+    else {
+        $app->response()->setStatus(400);
+        echo "Malformat JSON";
+    }
 });
 
 //TODO move it to a DAO class

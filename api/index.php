88c0bf05d939a7c5805bf46fca4b0ab9b8f4e018
Edy Segura <edysegura@gmail.com>
2015-11-11 00:03:38 +0000

    Added get by id

--- api/index.php
+++ api/index.php
@@ -16,11 +16,26 @@ $app->get('/tasks', function() use ( $app ) {
     echo json_encode($tasks);
 });
 
+$app->get('/tasks/:id', function($id) use ( $app ) {
+    $tasks = getTasks();
+    $index = array_search($id, array_column($tasks, 'id'));
+    
+    if($index > -1) {
+        $app->response()->header('Content-Type', 'application/json');
+        echo json_encode($tasks[$index]);
+    }
+    else {
+        $app->response()->setStatus(404);
+        echo "Not found";
+    }
+});
+
 //TODO move it to a DAO class
 function getTasks() {
     $tasks = array (
         array('id'=>1,'description'=>'Learn REST','done'=>false),
-        array('id'=>2,'description'=>'Learn JavaScript','done'=>false)
+        array('id'=>2,'description'=>'Learn JavaScript','done'=>false),
+        array('id'=>3,'description'=>'Learn English','done'=>false)
     );
     return $tasks;
 }
